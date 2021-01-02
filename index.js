40fabb9307a584883f5ffe3ba70c6280b0753f70
Haoming Xia <haoming@caroline.hitronhub.home>
2021-01-01 17:25:36 -0800

    commit

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const server = http.createServer((req, res) => {
   // Set the response HTTP header with HTTP status and Content type
   res.writeHead(200, { "Content-Type": "text/plain" });
 
-  // Send response body "Hello World"
-  res.end("Hello World\n");
+  // Send response body
+  res.sendFile("dashboard.html");
 });
 
 // Print when server starts listening
